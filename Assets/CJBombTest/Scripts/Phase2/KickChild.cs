21f1c38da83b7200de987eee42aac5ecab7d50df
cjiino <157668087+cjiino@users.noreply.github.com>
2026-01-31 18:04:25 -0800

    Added kick child

---
+++ Assets/CJBombTest/Scripts/Phase2/KickChild.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using TMPro;
+
+public class KickChild : Puzzle
+{
+    [SerializeField] TMP_Text childText;
+    [SerializeField] int requiredChildren = 15;
+    int kickedChildren = 0;
+
+    void Start()
+    {
+        kickedChildren = 0;
+        childText.text = kickedChildren.ToString();
+    }
+
+    public void IncrementChildCounter()
+    {
+        kickedChildren++;
+        childText.text = kickedChildren.ToString();
+        if(kickedChildren >= requiredChildren)
+        {
+            base.Solve();
+        }
+    }
+}
